772efcb06c7655b3d202976f30cb2a1cce507f03
lamery24 <93548465+lamery24@users.noreply.github.com>
2022-11-09 11:51:40 +0000

    added api

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app=Flask(__name__)
 
+DL=[
+  {
+    'id':1,
+    'title':'Doha',
+    'country': 'QAT',
+    'date':'13 May 2022'
+  },
+    {
+    'id':2,
+    'title':'Birmingham',
+    'country': 'GBR',
+    'date':'21 May 2022'
+  },
+      {
+    'id':3,
+    'title':'Eugene',
+    'country': 'USA',
+    'date':'28 May 2022'
+  },
+]
+
 @app.route("/")
-def hello_world():
-  return render_template('home.html')
+def hp():
+  return render_template('home.html',
+                        dl=DL)
+
+@app.route("/api/dlsched")
+def dl_sched():
+  return jsonify(DL)
 
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
