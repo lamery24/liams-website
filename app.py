2e87fc3301021553b49e30c27dc1bc20d8837d01
lamery24 <93548465+lamery24@users.noreply.github.com>
2022-11-09 14:46:46 +0000

    format of pages, added custom error pages

--- app.py
+++ app.py
@@ -7,30 +7,50 @@ DL=[
     'id':1,
     'title':'Doha',
     'country': 'QAT',
-    'date':'13 May 2022'
+    'date':'13 May 2022',
+   'site':'https://doha.diamondleague.com/programme-results-doha/'
   },
     {
     'id':2,
     'title':'Birmingham',
     'country': 'GBR',
-    'date':'21 May 2022'
+    'date':'21 May 2022',
+      'site':'https://doha.diamondleague.com/programme-results-london/'
   },
       {
     'id':3,
     'title':'Eugene',
     'country': 'USA',
-    'date':'28 May 2022'
+    'date':'28 May 2022',
+        'site':'https://doha.diamondleague.com/programme-results-eugene/'
   },
 ]
 
 @app.route("/")
 def hp():
-  return render_template('home.html',
+  return render_template('index.html',
                         dl=DL)
+
+
 
 @app.route("/api/dlsched")
 def dl_sched():
   return jsonify(DL)
 
+#invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template("404.html"), 404
+
+#Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+  return render_template("500.html"), 500
+
+  
+@app.route("/elite-results/")
+def er():
+  return render_template('elite-results.html')
+  
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
